32bbded40149837cf5d3dc6d272e79352a6f4ed7
Muhammad Mominul Huque <mominul2082@gmail.com>
2020-10-29 23:04:19 +0600

    Initial code commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use run_script::{run, ScriptOptions};
+
+const SCRIPT: &str = include_str!("git-subtree.sh");
+
+fn main() {
+    let args: Vec<String> = std::env::args().skip(1).collect();
+    let (code, output, error) = run(SCRIPT, &args, &ScriptOptions::new()).unwrap();
+    println!("{}", output);
+    println!("{}", error);
+    std::process::exit(code);
+}
