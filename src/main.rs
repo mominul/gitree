bb04235cbc703741da4170443196846f9ffdb7b1
Muhammad Mominul Huque <mominul2082@gmail.com>
2020-10-31 21:12:46 +0600

    Don't output unnecessary newlines and references to `git subtree` Add cargo metadatas

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,12 @@ const SCRIPT: &str = include_str!("git-subtree.sh");
 
 fn main() {
     let args: Vec<String> = std::env::args().skip(1).collect();
-    let (code, output, error) = run(SCRIPT, &args, &ScriptOptions::new()).unwrap();
-    println!("{}", output);
-    eprintln!("{}", error);
+    let (code, mut output, error) = run(SCRIPT, &args, &ScriptOptions::new()).unwrap();
+
+    // Replace 'git subtree' with `gitree` in the output.
+    output = output.replace("git subtree", "gitree");
+
+    print!("{}", output);
+    eprint!("{}", error);
     std::process::exit(code);
 }
