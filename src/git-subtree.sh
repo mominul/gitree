613d84e201a211fd8faa6253506f629d31ae1644
Muhammad Mominul Huque <mominul2082@gmail.com>
2020-11-01 21:24:45 +0600

    Fix the recursive call bug sanitize the error output too

--- src/git-subtree.sh
+++ src/git-subtree.sh
@@ -1010,7 +1010,7 @@ cmd_push () {
 		repository=$1
 		refspec=$2
 		echo "git push using: " "$repository" "$refspec"
-		localrev=$(git subtree split --prefix="$prefix") || die
+		localrev=$($GITREE split --prefix="$prefix") || die
 		git push "$repository" "$localrev":"refs/heads/$refspec"
 	else
 		die "'$dir' must already exist. Try 'git subtree add'."
--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,18 @@ use run_script::{run, ScriptOptions};
 const SCRIPT: &str = include_str!("git-subtree.sh");
 
 fn main() {
-    let args: Vec<String> = std::env::args().skip(1).collect();
-    let (code, mut output, error) = run(SCRIPT, &args, &ScriptOptions::new()).unwrap();
+    let mut args = std::env::args();
+
+    // The script calls itself recursively, so give the path of this executable via environment variable.
+    let exe_path = args.next().unwrap();
+    std::env::set_var("GITREE", exe_path);
+
+    let args = args.collect();
+    let (code, mut output, mut error) = run(SCRIPT, &args, &ScriptOptions::new()).unwrap();
 
     // Replace 'git subtree' with `gitree` in the output.
     output = output.replace("git subtree", "gitree");
+    error = error.replace("git subtree", "gitree");
 
     print!("{}", output);
     eprint!("{}", error);
